5f5104916a07dec2b05a20dfe85aacfd64f1b5d8
HaroEduardo <eduardo.ro123545@gmail.com>
2019-08-22 13:31:02 -0500

    Autocompleate

--- sgc/src/main/webapp/assets/js/pages/ordenVaceado.js
+++ sgc/src/main/webapp/assets/js/pages/ordenVaceado.js
@@ -1,18 +1,37 @@
 $(document).ready(function () {
     $("#serie").prop("disabled", true);
+    $('#nombreE').autocomplete({
+        serviceUrl: 'vaceado/getEmpleados',
+        paramName: "nombre",
+        delimiter: ",",
+        transformResult: function (response) {
+            var data = JSON.parse(response);
+            console.log(data);
+            return {
+                //must convert json to javascript object before process
+                suggestions: $.map(data, function (item) {
+
+                    return {value: item[1] + " " + item[2] + " " + item[3], data: item[0]}; 
+                    $('#serie').val(item[4]);
+                })
+
+            };
+
+        }
+
+    });
+
 });//ready
 
-function btnOrdenVaceado(){
+function btnOrdenVaceado() {
     $("#agregarV").css("display", "inline");
     $("#myModal").modal("show");
 }
 
-function btnAgregarMoldes(){
+function btnAgregarMoldes() {
     $("#myModal9").modal("show");
 }
 
-function cerrarModal(){
-     $("#myModal9").modal("hide");
+function cerrarModal() {
+    $("#myModal9").modal("hide");
 }
-
-
